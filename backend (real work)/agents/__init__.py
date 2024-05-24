dad141cdfc59ffe9d7f21ab3e70c40abb8b8160c
lstuma <lstuma@proton.me>
2024-05-24 10:02:33 +0200

    :coffin:

--- backend (real work)/agents/__init__.py
+++ backend (real work)/agents/__init__.py
@@ -1,17 +1,1 @@
 import asyncio
-
-from metagpt.context import Context
-from metagpt.roles.product_manager import ProductManager
-from metagpt.logs import logger
-
-
-async def main():
-    msg = "Write a PRD for a tetris game"
-    context = Context()  # The session Context object is explicitly created, and the Role object implicitly shares it automatically with its own Action object
-    role = ProductManager(context=context)
-    while msg:
-        msg = await role.run(msg)
-        logger.info(str(msg))
-
-if __name__ == '__main__':
-    asyncio.run(main())
